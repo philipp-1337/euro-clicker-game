2785f8c42f4d958a4cf2caa0b9cb577340c98f82
fabiokay <fabiokempf@gmail.com>
2025-05-13 17:11:22 +0200

    add sound to manager buy

--- src/hooks/useManagers.js
+++ src/hooks/useManagers.js
@@ -1,6 +1,12 @@
+import { useCallback } from 'react';
+
+// Load sound effect
+const clickSound = new Audio('/sounds/manager-sound.mp3');
+
 export default function useManagers(money, setMoney, managers, setManagers, ensureStartTime) {
-  function buyManager(index, cost) {
+  const buyManager = useCallback((index, cost) => {
     if (money >= cost && !managers[index]) {
+      clickSound.play(); // Play sound effect when a manager is purchased
       ensureStartTime?.();
       setMoney(prevMoney => prevMoney - cost);
       setManagers(prevManagers => {
@@ -9,7 +15,7 @@ export default function useManagers(money, setMoney, managers, setManagers, ensu
         return newManagers;
       });
     }
-  }
+  }, [money, managers, setMoney, setManagers, ensureStartTime]);
 
   return { buyManager };
 }
