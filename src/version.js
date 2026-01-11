8c26468150147935e3d9717c4c4d62d2e0474e28
Philipp Kanter <philippkanter@gmail.com>
2026-01-11 10:28:48 +0100

    feat: update version to 0.4.5

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 // Dieses File wird automatisch generiert. Nicht manuell editieren!
 // Enthält die aktuelle Version aus package.json
-export const APP_VERSION = "0.4.4";
+export const APP_VERSION = "0.4.5";
